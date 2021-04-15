db0965058e28e4fbf474ff9823b7bcc1314dc894
NHappyFox <eduardowna2020@yandex.ru>
2021-04-16 00:39:35 +0800

    Для шестой домашней работы

--- client/src/main/java/ru/geekbrains/march/chat/client/Main.java
+++ client/src/main/java/ru/geekbrains/march/chat/client/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-
+//hdfhftyhturtujrtu
     public static void main(String[] args) {
         launch(args);
     }
